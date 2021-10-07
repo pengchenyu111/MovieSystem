261ef46562ebefcd87824d99ed2468d5ec47ccd0
pengchenyu <1106998960@qq.com>
2021-10-07 13:09:17 +0800

    用户评分模拟模块-添加Nacos发现

--- Movie-System-Mock/src/main/java/com/pcy/MockServerApplication.java
+++ Movie-System-Mock/src/main/java/com/pcy/MockServerApplication.java
@@ -1,16 +1,15 @@
 package com.pcy;
 
-import com.pcy.service.MovieDetailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author PengChenyu
  * @since 2021-10-06 13:32:23
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MockServerApplication {
 
     public static void main(String[] args) {
